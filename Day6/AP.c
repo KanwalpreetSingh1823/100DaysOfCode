ed47b37148688219e66b3600dd4308778ca32139
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-01-20 19:50:54 +0530

    AP_Series.c

---
+++ Day6/AP.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("How many times do you want to print numbers : ");
+    scanf("%d",&n);
+    int a;
+    printf("Enter the number from where you want to start : ");
+    scanf("%d",&a);
+    int b;
+    printf("What is the difference you want : ");
+    scanf("%d",&b);
+    for(int i=4; i<=(3*n+1); i=i+3){
+        printf("%d ",a);
+        a = a + b;
+    }
+    printf("...");
+    return 0;
+}
