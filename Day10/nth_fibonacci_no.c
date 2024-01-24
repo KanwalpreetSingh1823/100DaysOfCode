619239605a611983cc153aff8b855fdcf189a204
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-01-24 23:20:15 +0530

    nth_Fibonacci_no.c

---
+++ Day10/nth_fibonacci_no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    int a = 1;
+    int b = 1;
+    int c = 1;
+    for(int i=1; i<=n-2; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    printf("The %d fibonacci number is %d ",n,c);
+    return 0;
+}
