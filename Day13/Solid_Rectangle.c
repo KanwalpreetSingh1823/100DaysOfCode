2d1892f4f909a08c8930576f63ac44fc7b18be92
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-01-27 18:05:57 +0530

    Solid_rectangle.c

---
+++ Day13/Solid_Rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter the number of rows : ");
+    scanf("%d",&r);
+    printf("Enter the number of columns : ");
+    scanf("%d",&c);
+    for(int i=1; i<=r; i++){
+        for(int i=1; i<=c; i++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
