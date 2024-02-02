316f4860eeb92fbb4b6ac962ffb077f470ddd39f
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-02-02 21:14:06 +0530

    Expert_setter.c

---
+++ Day19/Expert_Setter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int x,y,c;
+        scanf("%d %d",&x,&y);
+        c=(y*100)/x;
+        if(c>=50){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
