b20e6cb49d5d63a0baf83b3aa6f90d7290a57877
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-02-08 19:59:04 +0530

    Pointer_Problem.c

---
+++ Day25/Pointer_Problem.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void update(int* x,int* y){
+    int temp = *x;
+    *x = *x + *y;
+    *y = *y - temp;
+    if(*y<0){
+        *y = *y * (-1);
+    }
+}
+int main(){
+    int a,b;
+    scanf("%d\n%d",&a,&b);
+    update(&a,&b);
+    printf("%d\n%d",a,b);
+    return 0;
+}
