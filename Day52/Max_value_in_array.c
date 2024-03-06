9603f46e1621b747419e33d52e38ef09e78a2f24
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-03-06 19:38:14 +0530

    Maximum_value.c

---
+++ Day52/Max_value_in_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,c=1,max;
+    printf("Enter the number of elements : ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<=n-1;i++){
+        printf("Enter number %d : ",c);
+        scanf("%d",&arr[i]);
+        c++;
+    }
+    max = arr[0];
+    for(int i=0;i<=n-1;i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+    }
+    printf("Maximum value = %d",max);
+    return 0;
+}
