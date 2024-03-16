66ab5835327f602c4fe36763a6fb6dab9f3732b0
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-03-16 23:37:34 +0530

    Good_Program.c

---
+++ Day59/Good_Program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int t;                        // Codechef Problem
+    scanf("%d",&t);
+    while(t--){
+        int x;
+        scanf("%d",&x);
+        if(x%4==0) printf("Good\n");
+        else printf("Not Good\n");
+    }
+    return 0;
+}
