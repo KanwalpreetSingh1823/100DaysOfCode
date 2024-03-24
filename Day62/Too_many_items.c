0718a49c077857d213cc2cae224d7b199b8556ab
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-03-24 23:40:06 +0530

    Adding Comments

--- Day62/Too_many_items.c
+++ Day62/Too_many_items.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
     int t;              // Codechef Problem
-    scanf("%d",&t);
+    scanf("%d",&t);     // Taking Input
     while(t--){
-        int n,c=0;
+        int n,c=0;    
         scanf("%d",&n);
-        for(int i=1 ; i<=n; i=i+10){
+        for(int i=1 ; i<=n; i=i+10){       // Increment i by 10 bec' one polybag can contain upto 10 items
             c++;
         }
-        printf("%d\n",c);
+        printf("%d\n",c); // Print number of polybags.
     }
     return 0;
 }
