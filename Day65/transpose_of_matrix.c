71c40c75daac0cbfa47e903e604c172f7e2de391
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-03-28 23:06:44 +0530

    Adding comments to transpose of matrix.c

--- Day65/transpose_of_matrix.c
+++ Day65/transpose_of_matrix.c
@@ -5,18 +5,18 @@ int main(){
     scanf("%d %d",&r,&c);
     int arr[r][c];
     for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
+        for(int j=0; j<c; j++){   // Taking input or arr[][] matrix.
             scanf("%d",&arr[i][j]);
         }
     }
     int brr[c][r];
     for(int i=0; i<c; i++){   // Storing into another matrix
-        for(int j=0; j<r; j++){
+        for(int j=0; j<r; j++){    // and change the values.
             brr[i][j] = arr[j][i];
         }
     }
-    printf("\n");
-    for(int i=0; i<c; i++){
+    printf("\n");   
+    for(int i=0; i<c; i++){    // Print the values of brr[][] matrix.
         for(int j=0; j<r; j++){
             printf("%d ",brr[i][j]);
         }
