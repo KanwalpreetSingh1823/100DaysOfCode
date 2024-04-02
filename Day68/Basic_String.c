9d416901c3e8b32a414c735e7c11ab8484a2d1d9
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-04-02 22:29:10 +0530

    Adding comments in Basic string

--- Day68/Basic_String.c
+++ Day68/Basic_String.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-    char arr[] = "Hello world\0";
+    char str[] = "Hello world\0";   // Here we use Null Character.
     int i=0;
-    while(arr[i]!='\0'){
-        printf("%c",arr[i]);
+    while(str[i]!='\0'){            // Loop will run upto the index of Null Character.
+        printf("%c",str[i]);
         i++;
     }
     return 0;
