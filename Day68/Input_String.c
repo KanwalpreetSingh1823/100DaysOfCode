7216900dd94157844a364a1a5f7a4034c50964a4
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-04-02 22:29:46 +0530

    Adding comments in Input String.c

--- Day68/Input_String.c
+++ Day68/Input_String.c
@@ -4,7 +4,7 @@ int main(){
     char str[40];
     printf("Enter string : ");
     // scanf("%[^\n]s",str);
-    gets(str);
+    gets(str);    // Use of a get function for input a string.
     printf("\n");
     printf("%s",str);
     return 0;
