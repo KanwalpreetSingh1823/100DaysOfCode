5995815a4f584bae98cd48f261d35c0f9822385c
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-04-08 22:14:24 +0530

    Adding_comments_in_string_pointer.c

--- Day70/String_Pointer.c
+++ Day70/String_Pointer.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char *ptr = "Hello World";
-    ptr = "Great";
+    char *ptr = "Hello World";  // ptr poimts to the first letter of string
+    ptr = "Great";    // Now it points to the first letter of other string
     printf("%s\n",ptr);
     // char str[] = "Check";
     // *ptr = str;
