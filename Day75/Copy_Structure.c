b0e93acc339cde66fdb6d468208a85f27702ff94
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-04-13 20:40:21 +0530

    Copy_of_Structure.c

---
+++ Day75/Copy_Structure.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    typedef struct Book{
+        char name[25];
+        int pages;
+    } Book;
+
+    Book B1,B2;
+    B1.pages = 300;
+    strcpy(B1.name,"Greaves");
+
+    B2 = B1;  //Coping one structure into another
+    printf("%s\n",B2.name);
+    printf("%d",B2.pages);
+    return 0;
+}
