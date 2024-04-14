0b2d1f9a3b6c8baa18da6b324f3b87cc83236f2c
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-04-14 22:45:10 +0530

    Adding Comments to Copy_Structure.c

--- Day75/Copy_Structure.c
+++ Day75/Copy_Structure.c
@@ -10,8 +10,8 @@ int main(){
     B1.pages = 300;
     strcpy(B1.name,"Greaves");
 
-    B2 = B1;  //Coping one structure into another
-    printf("%s\n",B2.name);
+    B2 = B1;  //Coping one structure into another and it creates a deep copy.
+    printf("%s\n",B2.name); // Printing attributes of B2
     printf("%d",B2.pages);
     return 0;
 }
