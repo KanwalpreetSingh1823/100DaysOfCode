6b52c11a0e302f2f07dc69ec254f8d8438aecf19
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-04-25 20:30:56 +0530

    SmallTriangle_LargeTriangle.c

---
+++ Day84/SmallTriangle_LargeTriangle.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct triangle                         // HackerRank Problem
+{
+	int a;
+	int b;
+	int c;
+};
+
+typedef struct triangle triangle;
+void sort_by_area(triangle* tr, int n) {
+    double s,p;
+    double arr[n];
+    triangle temp_area;   
+	for(int i=0; i<n; i++){
+        s=0;                            
+        p = (tr[i].a+tr[i].b+tr[i].c)/2.0;
+        s = sqrt(p * (p-tr[i].a) * (p-tr[i].b) * (p-tr[i].c));
+        arr[i] = s;                     
+    }
+    for(int i=1; i<n; i++){  
+        int j=i;
+        while(j>0 && arr[j] < arr[j-1]){
+            double temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            
+            temp_area = tr[j];
+            tr[j] = tr[j-1];
+            tr[j-1] = temp_area;
+            j--;
+        }
+    }
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	triangle *tr = malloc(n * sizeof(triangle));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
+	}
+	sort_by_area(tr, n);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
+	}
+	return 0;
+}
