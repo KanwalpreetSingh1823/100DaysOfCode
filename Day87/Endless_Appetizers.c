a9dfa5ba7e653fe2f758107a75781b9bc5ab1e65
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-05-02 23:24:57 +0530

    Adding Comments

--- Day87/Endless_Appetizers.c
+++ Day87/Endless_Appetizers.c
@@ -5,14 +5,14 @@ int main(void) {         // CodeChef Problem
     scanf("%d",&t);
     while(t--){
         int x,y,r;
-        scanf("%d %d %d",&x,&y,&r);
+        scanf("%d %d %d",&x,&y,&r); // Taking Input
         int c = 0;
-        int d = r/30;
+        int d = r/30;    // A.T.Q
         x += d;
         for(int i=0; i<x; i+=y){
             if(i%y==0) c++;
         }
-        printf("%d\n",c);
+        printf("%d\n",c);  //Print Count
     }
     return 0;
 }
